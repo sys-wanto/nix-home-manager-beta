831af22c9e44f1a8b1cdacf40183ea4e7cffa0e4
sys-wanto <nckdostrow@gmail.com>
2023-05-17 11:38:52 +0700

    fixing shell.nix

--- shell.nix
+++ shell.nix
@@ -1,7 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
+
 let
-  nodejsVersion = "16";
-  nodejs = pkgs.nodejs-16_*.build.override { };
+  nodejsVersion = "16.0.0";
+  nodejs = pkgs.nodejs-16_0;
+
 in pkgs.mkShell {
   buildInputs = with pkgs; [ nodejs ];
 }
